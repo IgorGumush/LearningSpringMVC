fe840f3461051ff800cc625021a83e5cbd373092
igorg1990 <igorg@auroraview.com>
2018-07-21 12:24:36 +0300

    Spring MVC - Hello World
    - Creating @Controller class (inherit from @component)
    - Add @RequestMapping which return the View name. (in
    /WEB-INF/view/<name>.jsp)
    - Create a webpage

---
+++ spring-mvc-demo/src/com/mvc/spring/webapp/HomeController.java
@@ -0,0 +1,19 @@
+package com.mvc.spring.webapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//Telling spring that its and MVC Controller.
+// @Controller Extends from @Component 
+@Controller
+public class HomeController {
+	
+	//Should handle all type of http GET/POST requests
+	@RequestMapping("/")
+	public String showPage() {
+		//return the page name (spring would add the prefix and postfix 
+		// as described in the servlet xml)
+		return "main-menu";
+	}
+	
+}
